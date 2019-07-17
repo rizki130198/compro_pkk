d734cb6975fa682db54a9d3e0bd5717913542796
rizki130198 <rizkiajja130198@gmail.com>
2019-07-17 13:38:55 +0700

    dropdown dan edit paging

--- application/views/include/head.php
+++ application/views/include/head.php
@@ -1,4 +1,83 @@
 <!-- header section start -->
+<style type="text/css">
+ul li.drop {
+	position: relative;
+}
+
+ul li a:hover {
+	color: #eee;
+}
+
+.dropOut .triangle {
+	width: 0;
+	height: 0;
+	position: absolute;
+	border-left: 8px solid transparent;
+	border-right: 8px solid transparent;
+	border-bottom: 8px solid white;
+	top: -8px;
+	left: 50%;
+	margin-left: -8px;
+}
+.dropdownContain {
+	width: 160px;
+	position: absolute;
+	z-index: 2;
+	left: 50%;
+	margin-left: -80px; /* half of width */
+	top: -400px;
+	margin-top: -35px;
+}
+.dropOut {
+	width: 160px;
+	background: white;
+	float: left;
+	position: relative;
+	margin-top: 0px;
+	opacity: 0;
+	-webkit-border-radius: 4px;
+	-moz-border-radius: 4px;
+	border-radius: 4px;
+	-webkit-box-shadow: 0 1px 6px rgba(0,0,0,.15);
+	-moz-box-shadow: 0 1px 6px rgba(0,0,0,.15);
+	box-shadow: 0 1px 6px rgba(0,0,0,.15);
+	-webkit-transition: all .1s ease-out;
+	-moz-transition: all .1s ease-out;
+	-ms-transition: all .1s ease-out;
+	-o-transition: all .1s ease-out;
+	transition: all .1s ease-out;
+}
+
+.dropOut ul {
+	float: left;
+	padding: 10px 0;
+}
+.dropOut ul li {
+	text-align: left;
+	float: left;
+	width: 135px;
+	padding: 5px;
+	margin: 0px 10px;
+	color: #777;
+	cursor: pointer;
+	-webkit-border-radius: 4px;
+	-moz-border-radius: 4px;
+	border-radius: 4px;
+	-webkit-transition: background .1s ease-out;
+	-moz-transition: background .1s ease-out;
+	-ms-transition: background .1s ease-out;
+	-o-transition: background .1s ease-out;
+	transition: background .1s ease-out;
+}
+
+.dropOut ul li:hover {
+	background: #f6f6f6;
+}
+
+ul li:hover a { color: white; }
+ul li:hover .dropdownContain { top: 65px; }
+ul li:hover .underline { border-bottom-color: #777; }
+ul li:hover .dropOut { opacity: 1; margin-top: 8px; }</style>
 <header class="header">
 	<div class="container">
 		<div class="row flexbox-center">
@@ -10,21 +89,23 @@
 			<div class="col-lg-10 col-md-9 col-6">
 				<div class="responsive-menu"></div>
 				<div class="mainmenu">
-					<ul id="primary-menu">
-						<?php foreach ($menu as $key): ?>
-							<li><a class="nav-link" href="#<?=$key->menu?>"><?=$key->menu?></a></li>
-						<?php endforeach ?>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Dropdown
-							</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-								<a class="dropdown-item" href="#">Action</a>
-								<a class="dropdown-item" href="#">Another action</a>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="#">Something else here</a>
+					<ul id="primary-menu" class="Navigation">
+						<li><a class="nav-link" href="#Beranda">Beranda</a></li>
+						<li><a class="nav-link" href="#Berita">Berita</a></li>
+						<li class="drop">
+							<a href="#produk">Produk <i class="caret"></i></a>
+							<div class="dropdownContain">
+								<div class="dropOut">
+									<div class="triangle"></div>
+									<ul>
+										<li>PKK Mark</li>
+										<li>Simpan Pinjam</li>
+									</ul>
+								</div>
 							</div>
 						</li>
+						<li><a class="nav-link" href="#Hubungi Kami">Hubungi Kami</a></li>
+						<li><a class="nav-link" href="#Tentang">Tentang</a></li>
 					</ul>
 				</div>
 			</div>
