1ab8a11ab05d57adc959ec778510fe1d06d2abe5
rizkiajja130198 <rizkiajja130198@gmail.com>
2019-08-25 14:05:41 +0700

    update lagi

---
+++ application/views/pages/learn_more.php
@@ -0,0 +1,94 @@
+<!-- breadcrumb area start -->
+<section class="hero-area breadcrumb-area">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<div class="hero-area-content">
+					<h1>Learn More</h1>
+					<ul>
+						<li><a href="index.html">Beranda</a></li>
+						<li><a href="blog.html">Learn More</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- breadcrumb area end -->
+
+<!-- learn more section start -->
+<section class="blog-area blog-page" id="">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="main-timeline">
+					<a href="#" class="timeline">
+						<div class="timeline-icon">
+							<div style="background-color: #ec496e;width: 100px;height: 100px;border-radius: 100%;">
+								<img src="<?php echo base_url('assets/img/logo/ektp.png'); ?>" width="68%" style="margin-top:28px;">
+							</div>
+						</div>
+						<div class="timeline-content" style="margin: 100px 0 0;">
+							<h3 class="title">Membawa KTP</h3>
+							<p class="description">
+								Salah satu persyaratan Menjadi Anggota Koperasi PKK Melati Jaya adalah warga negara Indonesia dan bertempat tinggal di Sekitar JABODETABEK, hal ini dibuktikan dengan menunjukan KTP yang berlaku
+							</p>
+						</div>
+					</a>
+					<a href="#" class="timeline">
+						<div class="timeline-icon">
+							<div style="background-color: #f9850f;width: 100px;height: 100px;border-radius: 100%;">
+								<img src="<?php echo base_url('assets/img/logo/icon2.png'); ?>" width="55%" style="margin-top:20px;">
+							</div>
+						</div>
+						<div class="timeline-content" style="margin: 100px 0 0;">
+							<h3 class="title">Mengisi Formulir</h3>
+							<p class="description">
+								Calon Anggota mengisi formulir pendaftaran yang berisi data pribadi dan membubuhkan Tanda tangan dan cap jempol kiri pada buku daftar anggota yang disediakan serta foto berwarna 3x4 sebanyak 2 lembar
+							</p>
+						</div>
+					</a>
+					<a href="#" class="timeline">
+						<div class="timeline-icon">
+							<div style="background-color: #8fb800;width: 100px;height: 100px;border-radius: 100%;">
+								<img src="<?php echo base_url('assets/img/logo/iconcoin.png'); ?>" width="75%" style="margin-top:22px;">
+							</div>
+						</div>
+						<div class="timeline-content" style="margin: 120px 0 0;">
+							<h3 class="title">Melakukan Pelunasan Simpanan</h3>
+							<p class="description">
+								Setiap calon anggota koperasi melunasi simpanan pokok sebesar 1 juta Rupiah, dibayar 1x selama menjadi dan bisa dicicil selama 10 bulan. Serta pelunasan simpanan wajib sebesar 25ribu Rupiah dibayar setiap bulannya dan simpanan sukarela MINIMAL Rp 10.000 tiap bulannya
+							</p>
+						</div>
+					</a>
+					<a href="#" class="timeline">
+						<div class="timeline-icon">
+							<div style="background-color: #2fcea5;width: 100px;height: 100px;border-radius: 100%;">
+								<img src="<?php echo base_url('assets/img/logo/iconmessage.png'); ?>" width="70%" style="margin-top:18px;">
+							</div>
+						</div>
+						<div class="timeline-content" style="margin: 105px 0 0;">
+							<h3 class="title">Memenuhi Persyaratan Lain Yang Ditentukan Pengurus</h3>
+							<p class="description">
+								Calon anggota dianggap mengundurkan diri jika selama 10 (sepuluh) bulan terhitung sejak mendaftarkan diri belum juga memenuhi persyaratan 
+							</p>
+						</div>
+					</a>
+					<a href="#" class="timeline">
+						<div class="timeline-icon">
+							<div style="background-color: #1545b3;width: 100px;height: 100px;border-radius: 100%;">
+								<img src="<?php echo base_url('assets/img/logo/iconjabat.png'); ?>" width="75%" style="margin-top:28px;">
+							</div>
+						</div>
+						<div class="timeline-content" style="margin: 180px 0 0;">
+							<h3 class="title">Selamat Bergabung.....!!!</h3>
+							<p class="description">
+								Dengan memenuhi persyaratan diatas, anda telah resmi menjadi anggota koperasi PKK Melati Jaya dan wajib tunduk dan patuh serta mentaati AD/ART yang berlaku. Untuk dapat melakukan pinjaman, dana yang ada harus mengendap terlebih dahulu selama 1 bulan, dan maksimal pinjaman sebesar 3x dari total simpanan yang ada untuk pertama kali, dan maksimal 5x untuk pinjaman seterusnya   
+							</p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section><!-- learn more section end -->
