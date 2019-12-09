b2811b9c96d44b2040221ab6c562208f123ee0b7
rizki130198 <rizkiajja130198@gmail.com>
2019-12-09 15:15:12 +0700

    slider

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,48 +2,19 @@
 <!-- hero area start -->
 <section class="hero-area" id="home">
 	<div class="hero-area-slider">
-		<div class="hero-area-single-slide">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<!-- <?php foreach ($home as $beranda) { ?>
-						<div class="hero-area-content">
-							<h1>Selamat Datang !</h1>
-							<div class="garis-header"></div>
-							<h3 class="desc-home"><?=$beranda->deskripsi?></h3>
-							<a href="#" class="appao-btn-started appao-btn">Get Started</a>
-							<a href="<?php echo site_url('learn_more'); ?>" class="appao-btn">Pelajari lebih lanjut</a>
-						</div>
-						<?php } ?> -->
-						<div class="hero-area-content">
-							<img src="<?php echo base_url('assets/img/back1.jpeg'); ?>" style="height:400px;border-radius: 12px;">
+		<?php foreach ($home as $beranda) { ?>
+			<div class="hero-area-single-slide">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="hero-area-content">
+								<img src="<?php echo base_url('assets/img/'.$beranda->gambar); ?>" style="height:400px;border-radius: 12px;">
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="hero-area-single-slide">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="hero-area-content">
-							<img src="<?php echo base_url('assets/img/details.jpg'); ?>" style="height:400px;border-radius: 12px;">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="hero-area-single-slide">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<div class="hero-area-content">
-							<img src="<?php echo base_url('assets/img/download-bg.jpg'); ?>" style="height:400px;border-radius: 12px;">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<?php } ?>
 	</div>
 </section>
 <!-- hero area end -->
@@ -125,17 +96,17 @@
 					<div class="carousel-item active">
 						<div class="row">
 							<?php foreach ($pengurus as $team) {?>
-							<div class="col-lg-3 col-md-6 col-sm-6">
-								<div class="our-team">
-									<div class="pic">
-										<img src="<?php echo base_url('assets/anggota/home/'.$team->foto); ?>">
-									</div>
-									<div class="team-content">
-										<h3 class="title"><?=$team->nama?></h3>
-										<span class="post"><?=$team->jabatan?></span>
+								<div class="col-lg-3 col-md-6 col-sm-6">
+									<div class="our-team">
+										<div class="pic">
+											<img src="<?php echo base_url('assets/anggota/home/'.$team->foto); ?>">
+										</div>
+										<div class="team-content">
+											<h3 class="title"><?=$team->nama?></h3>
+											<span class="post"><?=$team->jabatan?></span>
+										</div>
 									</div>
 								</div>
-							</div>
 							<?php } ?>
 						</div>
 						<!--.row-->
@@ -188,24 +159,24 @@
 			</div>
 			<!--.Carousel-->
 		</div>
-</section>
+	</section>
 
-<!-- counter section start -->
-<div style="background: url(assets/img/footer-grey.PNG) center center no-repeat;background-size: cover;width: 100%;height:64px;line-height: 0;"></div>
-<section style="padding-top: 20px;padding-bottom:50px;background-color: #dedfe1;">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-8 col-xs-12">
-				<div style="text-align: left;padding-right: 25px;">
-					<h2 style="margin-bottom: 20px;font-weight: 100;">Ayo mulai berlangganan dengan <br>Koperasi PKK Melati Jaya</h2>
+	<!-- counter section start -->
+	<div style="background: url(assets/img/footer-grey.PNG) center center no-repeat;background-size: cover;width: 100%;height:64px;line-height: 0;"></div>
+	<section style="padding-top: 20px;padding-bottom:50px;background-color: #dedfe1;">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-xs-12">
+					<div style="text-align: left;padding-right: 25px;">
+						<h2 style="margin-bottom: 20px;font-weight: 100;">Ayo mulai berlangganan dengan <br>Koperasi PKK Melati Jaya</h2>
+					</div>
+				</div>
+				<div class="col-md-4 col-xs-12">
+					<a href=""><button class="btn btn-subsribe btn-lg btn-block">Mulai Berlangganan</button></a>
 				</div>
 			</div>
-			<div class="col-md-4 col-xs-12">
-				<a href=""><button class="btn btn-subsribe btn-lg btn-block">Mulai Berlangganan</button></a>
-			</div>
 		</div>
-	</div>
-</section>
-<!-- counter section end -->
+	</section>
+	<!-- counter section end -->
 
 <!-- footer section start -->
